e2e816cc9851a34907f43c54f19c908c18a5b71a
Pete Benassi <benassipb@gmail.com>
2022-10-04 12:52:11 -0700

    added emailjs for working contact form

--- src/About.js
+++ src/About.js
@@ -2,29 +2,54 @@ import React from "react";
 import bmjlogo from "./images/bmjlogo.jpg";
 import bitmoji from "./images/bitmoji.png";
 import linkedIn from "./images/linkedin.png";
+import ContactUs from "./Contact";
 
 function About() {
   return (
     <div className="about-container">
       <div className="about-page">
-        <h2 className="about-welcome">Welcome</h2>
-        <p className="about-tagline ">
-          a blog on learning JavaScript, building meaningful projects, and
-          programming with purpose
-        </p>
-      </div>
+        <nav className="about-word-titles">
+          <a className="about-word-title" href="#welcome">
+            welcome
+          </a>
+          <a className="about-word-title bold" href="#purpose">
+            purpose
+          </a>
+          <a className="about-word-title" href="#values">
+            values
+          </a>
+          <a className="about-word-title" href="#backstory">
+            backstory
+          </a>
+          <a className="about-word-title bold" href="#philosophy">
+            philosophy{" "}
+          </a>
+          <a className="about-word-title" href="#resources">
+            resources
+          </a>
+          <a className="about-word-title bold" href="#contact">
+            contact
+          </a>
+          <a className="about-word-title" href="#thanks">
+            thanks
+          </a>
+        </nav>
 
-      <div className="about-body">
-        <p className="about-p">
+        <div className="about-welcome-section about-section " id="welcome">
+          <h2 className="about-headline">Welcome</h2>
+          <p className="about-tagline ">
+            a blog on learning JavaScript, building meaningful projects, and
+            programming with purpose
+          </p>
+        </div>
+
+        <p>
           After working for ten years in environmental education, international
           education, and youth development, I began learning to code.
         </p>
-
-        <p className="about-p">
+        <p>
           I now work on websites, apps, front-end design, and cloud development.
         </p>
-
-        <p className="about-p">Thanks for stopping by,</p>
 
         <div className="about-name-photo-container">
           <p className="about-p bold font-size-1-5rem cursive">Peter Benassi</p>
@@ -36,6 +61,35 @@ function About() {
             <img className="about-link" src={linkedIn} alt="logo" />
           </a>
         </div>
+      </div>
+
+      <div className="about-purpose-section about-section" id="purpose">
+        <h2 className="about-headline">Purpose</h2>
+      </div>
+
+      <div className="about-values-section about-section" id="values">
+        <h2 className="about-headline">Values</h2>
+      </div>
+
+      <div className="about-backstory-section about-section" id="backstory">
+        <h2 className="about-headline">Backstory</h2>
+      </div>
+
+      <div className="about-philosophy-section about-section" id="philosophy">
+        <h2 className="about-headline">Philosophy</h2>
+      </div>
+
+      <div className="about-resources-section about-section" id="resources">
+        <h2 className="about-headline">Resources</h2>
+      </div>
+
+      <div className="about-contact-section about-section" id="contact">
+        <h2 className="about-headline">Contact</h2>
+        <ContactUs />
+      </div>
+
+      <div className="about-thanks-section about-section" id="thanks">
+        <h2 className="about-headline">Thanks</h2>
       </div>
     </div>
   );
---
+++ src/Contact.js
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9o3ohqy",
+        "template_6hn45gg",
+        form.current,
+        "47Ib2n16C586Cl33f"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="about-form-container">
+      <form ref={form} onSubmit={sendEmail} className="about-contact-form">
+        <label className="about-contact-label">Name</label>
+        <input type="text" name="user_name" className="about-contact-input" />
+        <label className="about-contact-label">Email</label>
+        <input type="email" name="user_email" className="about-contact-input" />
+        <label className="about-contact-label">Message</label>
+        <textarea
+          name="message"
+          className="about-contact-input about-contact-message"
+        />
+        <input type="submit" value="Send" className="about-contact-submit" />
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
