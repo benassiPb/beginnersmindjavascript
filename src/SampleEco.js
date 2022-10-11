2518396d595ceeff22a9295e74d07427b7687015
Pete Benassi <benassipb@gmail.com>
2022-10-11 13:58:41 -0700

    eco page complete

--- src/SampleEco.js
+++ src/SampleEco.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ecocover from "./images/ecocover.png";
+import ecocover2 from "./images/ecocover2.png";
+import ecocover3 from "./images/ecocover3.png";
+import ecocover4 from "./images/ecocover4.png";
+import ecocover5 from "./images/ecocover5.png";
 import undrawcook from "./images/undrawcook.png";
 import undrawpeople from "./images/undrawpeople.jpg";
 import undrawstars from "./images/undrawstars.png";
-import undrawnature from "./images/undrawnature.png";
 import undrawgarden from "./images/undrawgarden.png";
-import undrawteam from "./images/undrawteam.png";
 import undrawcontact from "./images/undrawcontact.png";
 import undrawmeditation from "./images/undrawmeditation.png";
 import {
@@ -23,7 +25,6 @@ function SampleEco() {
     <div className="sample-sites-page">
       <div className="eco-page">
         {/* nav */}
-
         <div className="eco-nav">
           <div className="eco-nav-title">
             <h1>eco</h1>
@@ -36,17 +37,13 @@ function SampleEco() {
             <a className="eco-nav-item" href="#programs">
               programs
             </a>
-            <a className="eco-nav-item" href="#getinvolved">
-              get involved
-            </a>
-            <a className="eco-nav-item" href="#contactus">
+
+            <a className="eco-nav-item" href="#contact">
               contact us
             </a>
           </div>
         </div>
-
         {/* hero illustrated */}
-
         <div className="eco-hero-section-container">
           <div className="eco-hero-section">
             <div className="eco-hero-illustration-container">
@@ -75,36 +72,34 @@ function SampleEco() {
             </div>
           </div>
         </div>
-
-        {/* image */}
+        {/* image
         <div className="eco-mobile-image-container">
           <img src={ecocover} alt="eco" className="eco-mobile-image" />
-        </div>
-
+        </div> */}
         {/* <div className="eco-wrapper">
           <div className="eco-wrapper-inner"></div>
         </div> */}
-
         {/* about */}
-
-        <div className="eco-illustration-center-container ">
+        {/* <div className="eco-illustration-center-container ">
           <img
             src={undrawmeditation}
             alt="woman meditating"
             className="eco-illustration-center"
-          />
-        </div>
-
+          /> */}
+        {/* </div> */}
         <div id="about" className="eco-about-section-container">
-          <div className="eco-about-section">
+          <div className="eco-missions-section">
             <h2 className="eco-about-subtitle">mission</h2>
-            <p className="eco-about-text">
-              At Eco, we believe community is at the heart of everything we do.
-              Since 2022, we have grown and locally distributed more than 10,000
-              pounds of organic produce using sustainable farming principles. We
-              supply produce to local restaurants, sell at farmer's market, and
-              we also offer diverse range of programs for our community.
-            </p>
+            <div className="eco-about-text-container">
+              <p className="eco-about-text">
+                At Eco, we believe community is at the heart of everything we
+                do. Since 2022, we have grown and locally distributed more than
+                10,000 pounds of organic produce while following sustainable
+                farming principles. We supply produce to local restaurants, sell
+                at farmers' markets, and we also offer a wide range of
+                programming for our community.
+              </p>
+            </div>
 
             {/* team */}
 
@@ -132,17 +127,13 @@ function SampleEco() {
 
               <div className="eco-team-card">
                 <img src={ecoperson2} alt="man" className="eco-team-image" />
-                <h3>Pedro</h3>
+                <h3>Mike</h3>
                 <p>Program Coordinator</p>
               </div>
             </div>
           </div>
         </div>
-
-        {/* the land */}
-
         {/* programs */}
-
         <div className="eco-illustration-center-container ">
           <img
             src={undrawstars}
@@ -150,24 +141,38 @@ function SampleEco() {
             className="eco-illustration-center"
           />
         </div>
-
         <div id="programs">
-          <h2 className="eco-about-subtitle">programs</h2>
-          <div className="eco-programs-cards">
-            <div className="eco-programs-card">
-              <h2>community share</h2>
-            </div>
-            <div className="eco-programs-card">
-              <h2>youth summer camp</h2>
-            </div>
-            <div className="eco-programs-card">
-              <h2>sunday farmer's market</h2>
+          <div className="eco-programs-section-container">
+            <h2 className="eco-about-subtitle">programs</h2>
+            <div className="eco-programs-cards">
+              <div className="eco-programs-card">
+                <img
+                  src={ecocover}
+                  alt=""
+                  className="eco-programs-card-image"
+                />
+                <h3>community share</h3>
+              </div>
+              <div className="eco-programs-card">
+                <img
+                  src={ecocover3}
+                  alt=""
+                  className="eco-programs-card-image"
+                />
+                <h3>youth summer camp</h3>
+              </div>
+              <div className="eco-programs-card">
+                <img
+                  src={ecocover2}
+                  alt=""
+                  className="eco-programs-card-image"
+                />
+                <h3>farmer apprenticeship</h3>
+              </div>
             </div>
           </div>
         </div>
-
         {/* get involved */}
-
         <div className="eco-illustration-center-container ">
           <img
             src={undrawcook}
@@ -175,15 +180,20 @@ function SampleEco() {
             className="eco-illustration-center"
           />
         </div>
-
         <div id="getinvolved">
           <h2 className="eco-about-subtitle">get involved</h2>
-          <h2 className="eco-involved-card">apprenticeship</h2>
-          <h2 className="eco-involved-card">volunteer</h2>
-          <h2 className="eco-involved-card">work with us</h2>
+          <div className="eco-programs-cards">
+            <div className="eco-programs-card">
+              <img src={ecocover5} alt="" className="eco-programs-card-image" />
+              <h3>volunteer</h3>
+            </div>
+            <div className="eco-programs-card">
+              <img src={ecocover4} alt="" className="eco-programs-card-image" />
+              <h3>work with us</h3>
+            </div>
+          </div>
         </div>
         {/* contact us*/}
-
         <div className="eco-illustration-center-container ">
           <img
             src={undrawcontact}
@@ -191,16 +201,14 @@ function SampleEco() {
             className="eco-illustration-center"
           />
         </div>
-
         <div id="contact">
-          <h3 className="coffee-contact-section-title">contact</h3>
-          <div className="coffee-contact-section">
-            <p className="coffee-contact-label">name</p>
-            <p className="coffee-contact-label">email</p>
-            <p className="coffee-contact-label">message</p>
+          <div className="eco-contact-section-container">
+            <h3 className="eco-contact-section-title">contact</h3>
+            <p className="eco-contact-label">name</p>
+            <p className="eco-contact-label">email</p>
+            <p className="eco-contact-label">message</p>
           </div>
         </div>
-
         <div className="coffee-footer">
           <span className="coffee-footer-item">
             <AiFillInstagram size={28} />
