aa0a9815f26de4d6a9f04caa7ed6c939cb6d9ee5
Pete Benassi <benassipb@gmail.com>
2022-11-22 11:28:59 -0800

    google

--- src/RoutesComponent.js
+++ src/RoutesComponent.js
@@ -12,12 +12,9 @@ import Posts from "./Posts";
 import About from "./About";
 import Home from "./Home";
 import Error from "./Error";
-import Services from "./Services";
-import Shop from "./Shop";
 import SampleCoffee from "./SampleCoffee";
 import SampleCreative from "./SampleCreative";
 import SampleYoga from "./SampleYoga";
-import SampleNonprofit from "./SampleNonprofit";
 import SampleEco from "./SampleEco";
 import SampleProfessional from "./SampleProfessional";
 import SampleBooks from "./SampleBooks";
@@ -30,14 +27,11 @@ function RoutesComponent() {
       <Route path="/posts/:postId" element={<Posts />} />
       <Route path="/projects/:projectId" element={<Projects />} />
       <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
       <Route path="/samplesites" element={<SampleSites />} />
-      <Route path="/shop" element={<Shop />} />
       <Route path="/coffee" element={<SampleCoffee />} />
       <Route path="/eco" element={<SampleEco />} />
       <Route path="/books" element={<SampleBooks />} />
       <Route path="/yoga" element={<SampleYoga />} />
-      <Route path="/nonprofit" element={<SampleNonprofit />} />
       <Route path="/creative" element={<SampleCreative />} />
       <Route path="/professional" element={<SampleProfessional />} />
       <Route path="/" element={<Home />} />
--- src/SampleNonprofit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SampleNonprofit() {
-  return (
-    <div className="sample-sites-page">SampleNonprofit</div>
-  )
-}
-
-export default SampleNonprofit
